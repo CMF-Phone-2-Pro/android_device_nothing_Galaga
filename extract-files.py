d65fa661db7c9abc53ea8244c0a7021cb5aef61b
iamlordutkarsh <lalbaake456@gmail.com>
2025-10-06 12:50:30 +0000

    Galaga: patch bluetooth audio blob to use android.hardware.audio.common-V2-ndk
    Signed-off-by: iamlordutkarsh <lalbaake456@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.power-V4-ndk.so', 'android.hardware.power-V2-ndk.so'),
     'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+    'vendor/lib64/vendor.mediatek.hardware.bluetooth.audio-V1-ndk.so': blob_fixup()
+        .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so')
         .replace_needed('libalsautils.so', 'libalsautils-v33.so'),
