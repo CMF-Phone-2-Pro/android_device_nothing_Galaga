383ecc2357d42936537488b849347b068db7d0cc
Alexander Koskovich <akoskovich@pm.me>
2025-10-15 10:14:04 +0000

    Galaga: Add `libnativewindow` to `libntcamskia`

--- extract-files.py
+++ extract-files.py
@@ -77,6 +77,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('liblog.so'),
     'vendor/lib64/libneuron_adapter_mc.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_describe'),
+    'vendor/lib64/libntcamskia.so': blob_fixup()
+        .add_needed('libnativewindow.so'),
     'vendor/lib64/mt6878/libpqconfig.so': blob_fixup()
         .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
 }  # fmt: skip
